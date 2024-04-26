c9ffae2a48bc2fc9bff544fb4a0786b3f61c1997
marcelo Francisco <marcelo1195.mhbf@gmail.com>
2024-04-26 10:57:50 -0300

    Initial commit

---
+++ src/main/java/dio/projeto_dio_CloudParking/ProjetoDioCloudParkingApplication.java
@@ -0,0 +1,13 @@
+package dio.projeto_dio_CloudParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDioCloudParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDioCloudParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/projeto_dio_CloudParking/ProjetoDioCloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.projeto_dio_CloudParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDioCloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
